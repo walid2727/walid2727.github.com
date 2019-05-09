293fe5581440ce9b88f89adbe38b22a3314ed6ac
walid2727 <walidayed2000@gmail.com>
2019-05-09 17:58:11 +0200

    first commit
    first commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("main-menu").style.width = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("main-menu").style.width = "0";
+  }
